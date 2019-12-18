26821a7d8daaca9f7d4cf73b973c00c52f86fca3
RickJinny <rickjinny@126.com>
2019-12-19 00:26:20 +0800

    insert
    insert

--- src/main/java/com/rick/jinmall/mapper/UserInfoMapper.java
+++ src/main/java/com/rick/jinmall/mapper/UserInfoMapper.java
@@ -4,6 +4,14 @@ import com.rick.jinmall.bean.UserInfo;
 
 public interface UserInfoMapper {
 
+    /**
+     * 获取用户信息
+     */
     UserInfo getUserInfo();
+
+    /**
+     * 增加用户信息
+     */
+    void insertUserInfo();
 
 }
