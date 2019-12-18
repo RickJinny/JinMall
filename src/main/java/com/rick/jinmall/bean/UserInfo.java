c2ac2a2550051992b8f1d518f50e0c478f896b23
RickJinny <rickjinny@126.com>
2019-12-19 00:19:13 +0800

    user info
    user info

--- src/main/java/com/rick/jinmall/bean/UserInfo.java
+++ src/main/java/com/rick/jinmall/bean/UserInfo.java
@@ -10,48 +10,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class UserInfo {
-
-    // 主键
-    private int id;
-
+    // id
+    private Integer id;
     // 姓名
     private String name;
-
+    // 性别: 1是男; 2是女
+    private Integer sex;
     // 年龄
-    private int age;
-
+    private Integer age;
+    // 生日
+    private String birthday;
+    // 用户名
+    private String account;
     // 密码
     private String password;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    // 用户手机号
+    private String userPhone;
+    // 地址
+    private String address;
+    // 用户微信
+    private String userWeChat;
+    // 用户qq
+    private String userQQ;
+    // 用户邮箱
+    private String userEmail;
 }
