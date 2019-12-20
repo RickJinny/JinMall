f7bf08ef3b27de700ce4182ed2ec6b56dc58b5ea
RickJinny <rickjinny@126.com>
2019-12-20 22:08:53 +0800

    update
    update

--- src/main/java/com/rick/jinmall/bean/user/CustomUser.java
+++ src/main/java/com/rick/jinmall/bean/user/CustomUser.java
@@ -1,14 +1,8 @@
 package com.rick.jinmall.bean.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class CustomUser extends UserInfo {
 
 }
