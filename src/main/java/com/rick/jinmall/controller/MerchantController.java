64aa29aef8422fbe8120d55f9d0fc4f50a830bfb
RickJinny <rickjinny@126.com>
2019-12-21 12:06:02 +0800

    update
    update

--- src/main/java/com/rick/jinmall/controller/MerchantController.java
+++ src/main/java/com/rick/jinmall/controller/MerchantController.java
@@ -1,10 +1,46 @@
 package com.rick.jinmall.controller;
 
+import com.rick.jinmall.bean.merchant.Merchant;
+import com.rick.jinmall.service.MerchantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/merchant")
 public class MerchantController {
 
+    @Autowired
+    private MerchantService merchantService;
+
+    @RequestMapping(value = "/addMerchant")
+    @ResponseBody
+    public void addMerchant(Merchant merchant) {
+        merchantService.addMerchant(merchant);
+    }
+
+    @RequestMapping(value = "/updateMerchant")
+    @ResponseBody
+    public void updateMerchant(Merchant merchant) {
+        merchantService.updateMerchant(merchant);
+    }
+
+    @RequestMapping(value = "/getMerchantById")
+    @ResponseBody
+    public Merchant getMerchantById(int id) {
+        return merchantService.getMerchantById(id);
+    }
+
+    @RequestMapping(value = "/updateMerchantAuditStatus")
+    @ResponseBody
+    public void updateMerchantAuditStatus(Merchant merchant) {
+        merchantService.updateMerchantAuditStatus(merchant);
+    }
+
+    @RequestMapping(value = "/updateMerchantSoldOut")
+    @ResponseBody
+    public void updateMerchantSoldOut(Merchant merchant) {
+        merchantService.updateMerchantSoldOut(merchant);
+    }
 }
