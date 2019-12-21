6c2c41c7dac1f458cbc9b675123e0259141a80db
RickJinny <rickjinny@126.com>
2019-12-21 21:57:33 +0800

    product
    product

--- src/main/java/com/rick/jinmall/controller/ProductController.java
+++ src/main/java/com/rick/jinmall/controller/ProductController.java
@@ -1,13 +1,16 @@
 package com.rick.jinmall.controller;
 
+import com.rick.jinmall.bean.product.Product;
 import com.rick.jinmall.bean.product.ProductType;
 import com.rick.jinmall.bean.product.ProductTypeVo;
+import com.rick.jinmall.bean.product.ProductVo;
 import com.rick.jinmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -51,5 +54,56 @@ public class ProductController {
     @ResponseBody
     public void deleteProductTypeById(int id) {
         productService.deleteProductTypeById(id);
+    }
+
+    @RequestMapping(value = "/addProduct")
+    @ResponseBody
+    public void addProduct(Product product) {
+        product.setCreateTime(new Date());
+        product.setSellNum(0L);
+        product.setAuditStatus(0);
+        productService.addProduct(product);
+    }
+
+    @RequestMapping(value = "/auditProduct")
+    @ResponseBody
+    public void auditProduct(int id, int auditStatus) {
+        productService.auditProduct(id, auditStatus);
+    }
+
+    @RequestMapping(value = "/getProductById")
+    @ResponseBody
+    public Product getProductById(int id) {
+        return productService.getProductById(id);
+    }
+
+    @RequestMapping(value = "/updateProduct")
+    @ResponseBody
+    public void updateProduct(Product product) {
+        productService.updateProduct(product);
+    }
+
+    @RequestMapping(value = "/deleteProductById")
+    @ResponseBody
+    public void deleteProductById(int id) {
+        productService.deleteProductById(id);
+    }
+
+    @RequestMapping(value = "/queryProductByVo")
+    @ResponseBody
+    public List<Product> queryProductByVo(ProductVo productVo) {
+        return productService.queryProductByVo(productVo);
+    }
+
+    @RequestMapping(value = "/updateProductByStatus")
+    @ResponseBody
+    public void updateProductByStatus(int id, int auditStatus) {
+        productService.updateProductByStatus(id, auditStatus);
+    }
+
+    @RequestMapping(value = "/queryProductByIds")
+    @ResponseBody
+    public List<Product> queryProductByIds(String keyWord) {
+        return productService.queryProductByIds(keyWord);
     }
 }
