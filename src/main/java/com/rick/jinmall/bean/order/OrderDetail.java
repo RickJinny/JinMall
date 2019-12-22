f83ee8758058ba2bea54efa6cb737ef9b08e5f0b
RickJinny <rickjinny@126.com>
2019-12-22 17:23:50 +0800

    order detail
    order detail

---
+++ src/main/java/com/rick/jinmall/bean/order/OrderDetail.java
@@ -0,0 +1,21 @@
+package com.rick.jinmall.bean.order;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class OrderDetail {
+    // id
+    private Long id;
+    // 订单id
+    private Long orderId;
+    // 商品id
+    private Long productId;
+    // 商家id
+    private Long merchantId;
+    // 交易数量
+    private Long tradeNum;
+    // 创建时间
+    private Date createTime;
+}
